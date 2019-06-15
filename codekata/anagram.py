1f02f0d45d1e3328cddf827ad9b5f31d3511483b
YaminiNarayanan-359 <51689973+YaminiNarayanan-359@users.noreply.github.com>
2019-06-15 23:22:38 +0530

    Create anagram.py

---
+++ codekata/anagram.py
@@ -0,0 +1,12 @@
+p=int(input())
+l=[]
+c=0
+n=0
+for i in range(0,p):
+    h=input()
+    g=list(h)
+    if ("k" in g and "a" in g and "b" in g and "a" in g and "l" in g and "i" in g):
+        l.append(n)
+for i in l:
+    c=c+1
+print(c)
